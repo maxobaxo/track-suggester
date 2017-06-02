89a1263f1e9270b982c57d4cd4ee983f6e7cbe5d
Max Scher <maxobaxo@gmail.com>
2017-06-02 10:38:04 -0700

    add variables to capture survey inputs

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $(".survey").submit(function() {
+    var focus = $("input:radio[name=job-focus]:checked").val();
+    var coSize = parseInt($("#co-size").val());
+    var artsy = $("#artsy").val();
+    var codeAge = $("#code-age").val();
+    var peewee = $("input:radio[name=peewee]:checked").val();
+
+
+
+  });
+});
