08c3d613d2f18a53f385f24e64868c68da0a3c30
Max Scher <maxobaxo@gmail.com>
2017-06-02 10:59:48 -0700

    add JS if-statement for css answer

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
   $(".survey").submit(function() {
+
+
     var focus = $("input:radio[name=job-focus]:checked").val();
     var coSize = parseInt($("#co-size").val());
     var artsy = $("#artsy").val();
     var codeAge = $("#code-age").val();
     var peewee = $("input:radio[name=peewee]:checked").val();
 
-
-
+    if (artsy >= 3 && focus === "design") {
+      $("#css").show();
+    }
   });
 });
